209c63dad0d07c63c04b179e8bc742736ee47b41
Your Name <you@example.com>
2026-09-12 16:53:46 +0900

    Add GitHub Actions build workflow

---
+++ build-win/CMakeFiles/ClickHitboxTrail.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ClickHitboxTrail.
+# This may be replaced when dependencies are built.
---
+++ build-win/_deps/nontype_functional-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vite/click-hitbox-trail/build-win/_deps/nontype_functional-src
+# Build directory: /home/vite/click-hitbox-trail/build-win/_deps/nontype_functional-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
